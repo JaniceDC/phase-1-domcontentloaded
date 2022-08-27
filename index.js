e02d7c30378487769e564a5c4dc85e5c20e6bcbd
JaniceDC <janice.dipayan.chen@gmail.com>
2022-08-27 11:12:23 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("text").innerHTML = "This is really cool!";
+});
+
+// target element id="text"
+// replace text with "This is really cool!"
+//textContent or innerHTML
